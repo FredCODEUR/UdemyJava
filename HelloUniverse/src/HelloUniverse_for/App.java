a12aabd2c67376a5e1d1a4ebe8170f259dc34328
FBretonABCDEV <fred.eks@orange.fr>
2023-12-09 05:00:59 +0100

    update

---
+++ HelloUniverse/src/HelloUniverse_for/App.java
@@ -0,0 +1,22 @@
+package HelloUniverse_for;
+
+public class App {
+    public static void main(String[] args){
+        int nbPlanetes;
+        for (nbPlanetes = 7;nbPlanetes<=9;nbPlanetes++) {
+            switch (nbPlanetes) {
+                case 7:
+                    System.out.println("\nOn sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes\n");
+                    break;
+                case 8:
+                    System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006\n");
+                    break;
+                case 9:
+                    System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8\n");
+                    break;
+                default:
+                    System.out.printf("Le programme ne peut pas fournir de résultat pour %d",nbPlanetes);
+            }
+        }
+    }
+}
---
+++ HelloUniverse/src/HelloUniverse_switchCase/App.java
@@ -0,0 +1,32 @@
+package HelloUniverse_switchCase;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args){
+        /* VARIABLES */
+
+        int nbPlanetes=0;
+
+        /* DEBUT PROGRAMME */
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Saisir un nombre de planètes entre 7 et 9 : ");
+        nbPlanetes = userInput.nextInt();
+
+        switch(nbPlanetes){
+
+            case 7 :
+                System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
+                break;
+            case 8 :
+                System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
+                break;
+            case 9 :
+                System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
+                break;
+            default:
+                System.out.printf( "Le programme ne peut pas fournir de résultat pour %d", nbPlanetes);
+        }
+
+        /* FIN PROGRAMME */
+    }
+}
